16a09ac356c90d9f4cbef4f9f1b84786399aaf46
Bkstone05 <bkstone42@tntech.edu>
2025-04-16 16:17:22 -0500

    Worked on setting up file and main

--- Main.cpp
+++ Main.cpp
@@ -1,1 +1,27 @@
+/*
+    Names: Lucy Dunn and Baylee Stone
+    Compiling Instructions: 
+    Dependencies: 
+    Output;
+    located: console
+*/
 
+int RecursiveMax(int[], int);
+
+int main()
+{
+
+}
+
+int RecursiveMax(int arr[], int elements)
+{
+    int max; 
+    if(elements == 1)
+    {
+        return arr[0];
+    }
+    else 
+    {
+        max = RecursiveMax(arr, elements-1);
+    }
+}
