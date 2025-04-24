bb4e288c9fb9d8e635dcf3c30f5788bf8ca112be
Bkstone05 <bkstone42@tntech.edu>
2025-04-23 20:39:13 -0500

    Fixed my boo boo

--- Main.cpp
+++ Main.cpp
@@ -1,13 +1,15 @@
 /*
     Names: Lucy Dunn and Baylee Stone
     Compiling Instructions: 
-    Dependencies: 
+    Dependencies: https://www.asciiart.eu/text-to-ascii-art
     Output:
     located: console
 */
 
-#include "outPut.h"
+#include <iostream>
+using namespace std;
 
+void title();
 int RecursiveMax(int[], int);
 
 int main()
@@ -24,14 +26,95 @@ int main()
         cout << "Element " << i << ": ";
         cin >> arr[i];
     }
-    decrease(arr, elements);
-    cout << "Ending the decrease phase!\n\n";
+    cout << "\nLets look at the decrease in a visual manner!";
+    cout << "\nTake note of the array decreasing by one each time!";
     max= RecursiveMax(arr, elements);
-    conquer(arr, elements);
-
-
+    cout << "\nThe final max of the array " ;
+    cout << "\nS[";
+    for(int i=0; i<elements; i++)
+    {
+      if(i != elements-1)
+      {
+        cout << arr[i] << ","; 
+      }
+      else
+      {
+        cout << arr[i];
+      }
+    }
+    cout << "] is " << max;
 }
 
+int RecursiveMax(int arr[], int elements)
+{
+    int max = 0; 
+
+    cout << "\nS[";
+    for(int i=0; i<elements; i++)
+    {
+      if(i != elements-1)
+      {
+        cout << arr[i] << ","; 
+      }
+      else
+      {
+        cout << arr[i];
+      }
+    }
+    cout << "]";
+    if(elements == 1)
+    {
+        cout << "\n\nn = 1"; 
+        cout << "\nThis means we return s[0] and start conquering!";
+        cout << "\ns[0] = " << arr[0] << endl; 
+        cout << "\nWe will now look at the conquer phase!";
+        return arr[0];
+    }
+    else 
+    {
+        
+        max = RecursiveMax(arr, elements-1);
+        cout << "\nThe current max is " << max << endl;
+
+        if(max>arr[elements-1])
+        {
+            cout << "\nSince the max (" << max << ") is less than the element " << arr[elements-1] << endl;
+            cout << "We will be returning the max!";
+            return max;
+        }
+        else 
+        {
+            cout << "\nSince the max (" << max << ") is not less than the element " << arr[elements-1] << endl;
+            cout << "We will be returning the element!";
+            return arr[elements-1];
+        }
+    }
+}
+
+void title()
+{ 
+  cout<<"\n\n   ____                          _                                           " ;
+  cout<<"  |  _ \\ ___  ___ _   _ _ __ ___(_)_   _____                                " << endl;
+  cout<<"  | |_) / _ \\/ __| | | | '__/ __| \\ \\ / / _ \\                               " << endl;
+  cout<<"  |  _ <  __/ (__| |_| | |  \\__ \\ |\\ V /  __/                               " << endl;
+  cout<<"  |_|_\\_\\___|\\___|\\__,_|_|  |___/_| \\_/ \\___|_              ___             "<<endl;
+  cout<<"  |  _ \\  ___  ___ _ __ ___  __ _ ___  ___  | |__  _   _   / _ \\ _ __   ___ "<<endl;
+  cout<<"  | | | |/ _ \\/ __| '__/ _ \\/ _` / __|/ _ \\ | '_ \\| | | | | | | | '_ \\ / _ \\ ";
+   cout<<"  | |_| |  __/ (__| | |  __/ (_| \\__ \\  __/ | |_) | |_| | | |_| | | | |  __/" << endl;
+    cout<<"  |____/ \\___|\\___|_|  \\___|\\__,_|___/\\___| |_.__/ \\__, |  \\___/|_| |_|\\___|" << endl;
+    cout<<"   __ _ _ __   __| |  / ___|___  _ __   __ _ _   _|___/ _ __               " << endl;
+    cout<<"  / _` | '_ \\ / _` | | |   / _ \\| '_ \\ / _` | | | |/ _ \\ '__|              " << endl;
+    cout<<" | (_| | | | | (_| | | |__| (_) | | | | (_| | |_| |  __/ |                 " << endl;
+    cout<<"  \\__,_|_| |_|\\__,_|  \\____\\___/|_| |_|\\__, |\\__,_|\\___|_|                 " << endl;
+    cout<<"                                          |_|              "      << endl;          
+  cout << "\nDuring this program you will see the workings of a decrease and conquer recursive algorithm!\n";
+  cout << "As we go along, please press enter to see each step!\n"; 
+  cout << "To start please enter in the size of your array: ";
+}
+
+
+
+/* This is a copy of our algorithm before I added in the extras for output 
 int RecursiveMax(int arr[], int elements)
 {
     int max; 
@@ -53,3 +136,4 @@ int RecursiveMax(int arr[], int elements)
         }
     }
 }
+*/
