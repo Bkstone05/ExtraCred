d9fc5069c8d7a3af4499121bbf78d1c9dad48319
Bkstone05 <bkstone42@tntech.edu>
2025-04-24 19:15:01 -0500

    Finished project

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,8 @@
 /*
     Names: Lucy Dunn and Baylee Stone
-    Compiling Instructions: 
+    Compiling Instructions: g++ Main.cpp then enter and then a enter to run 
     Dependencies: https://www.asciiart.eu/text-to-ascii-art
-    Output:
+    Output: Static and interactive 
     located: console
 */
 
@@ -16,21 +16,49 @@ int main()
 {
     int elements, max; 
     int* arr;
+    char cont;
 
     title(); 
+    do
+    {
+      cout << "To start please enter in the size of your array: ";
     cin >> elements; 
     arr = new int[elements]; 
-    cout << "Please enter in your elements below" << endl; 
+    cout << "Please enter in your elements below" << endl << endl; 
     for(int i =0; i<elements;i++)
     {
-        cout << "Element " << i << ": ";
+        cout << "\tElement " << i << ": ";
         cin >> arr[i];
     }
-    cout << "\nFrom now on when asked if you are ready to continue please enter 1 to proceed!";
-    cout << "\nLets look at the decrease in a visual manner!";
-    cout << "\nTake note of the array decreasing by one each time!";
+  
+    cout << "\nYour array is S[";
+    for(int i=0; i<elements; i++)
+    {
+      if(i != elements-1)
+      {
+        cout << arr[i] << ","; 
+      }
+      else
+      {
+        cout << arr[i];
+      }
+    }
+    cout << "]";
+    cout << "\nIf the array above is correct please enter in y for yes and n for no.\n" ; 
+    cin >> cont;
+    }while(cont != 'y' && cont != 'Y');
+    cout << "\nFrom now on when asked if you are ready to continue please enter y for yes to proceed!";
+    cout << "\n\nLets look at the decrease in a visual manner!" << endl;
+    cout << "Take note of the array decreasing by one each time!" << endl;
+    cout << "\nAre you ready to start? ";
+    cin >> cont;
+    while(cont != 'y' && cont != 'Y')
+    {
+      cout << "Please enter in y to continue "; 
+      cin >> cont; 
+    }
     max= RecursiveMax(arr, elements);
-    cout << "\nThe final max of the array " ;
+    cout << "\n\nThe final max of the array " ;
     cout << "\nS[";
     for(int i=0; i<elements; i++)
     {
@@ -45,16 +73,11 @@ int main()
     }
     cout << "] is " << max;
 }
-
+//----------------------------------------------------------------------------------
 int RecursiveMax(int arr[], int elements)
 {
     int max = 0; 
-    int cont; 
-
-    cout << "\n\nAre you ready to continue?";
-    cin >> cont;
-    if(cont == 1)
-    {
+    char cont; 
       cout << "\nS[";
       for(int i=0; i<elements; i++)
       {
@@ -75,12 +98,14 @@ int RecursiveMax(int arr[], int elements)
           cout << "\ns[0] = " << arr[0] << endl; 
           cout << "\nWe will now look at the conquer phase!";
           cout << "\nAgain take note of the increase each time in the array!";
-          cout << "\n\nAre you ready to continue?";
+          cout << "\n\nAre you ready to continue? ";
           cin >> cont;
-          if(cont == 1)
+          while(cont != 'y' && cont != 'Y')
           {
-            return arr[0];
+            cout << "\nPlease enter in y to proceed: ";
+            cin >> cont;
           }
+            return arr[0];
       }
       else 
       {
@@ -90,7 +115,7 @@ int RecursiveMax(int arr[], int elements)
 
           if(max>arr[elements-1])
           {
-              cout << "\nSince the max (" << max << ") is less than the element " << arr[elements-1] << endl;
+              cout << "\nSince the max (" << max << ") is not less than the element " << arr[elements-1] << endl;
               cout << "We will be returning the max!";
               cout << "\nS[";
               for(int i=0; i<elements; i++)
@@ -105,16 +130,11 @@ int RecursiveMax(int arr[], int elements)
                 }
               }
               cout << "]";
-              cout << "\n\nAre you ready to continue?";
-              cin >> cont;
-              if(cont ==1 )
-              {
                 return max;
-              }  
           }
           else 
           {
-              cout << "\nSince the max (" << max << ") is not less than the element " << arr[elements-1] << endl;
+              cout << "\nSince the max (" << max << ") is less than the element " << arr[elements-1] << endl;
               cout << "We will be returning the element!";
               cout << "\nS[";
               for(int i=0; i<elements; i++)
@@ -129,27 +149,23 @@ int RecursiveMax(int arr[], int elements)
                 }
               }
               cout << "]";
-              cout << "\n\nAre you ready to continue?";
-              cin >> cont;
-              if(cont ==1 )
-              {
+              
                 return arr[elements-1];
-              }
+            
           }
-      }
-    }  
+      } 
     return max;
 }
 
 void title()
 { 
-  cout<<"\n\n   ____                          _                                           " ;
+  cout<<"\n\n   ____                          _                                           " << endl ;
   cout<<"  |  _ \\ ___  ___ _   _ _ __ ___(_)_   _____                                " << endl;
   cout<<"  | |_) / _ \\/ __| | | | '__/ __| \\ \\ / / _ \\                               " << endl;
   cout<<"  |  _ <  __/ (__| |_| | |  \\__ \\ |\\ V /  __/                               " << endl;
   cout<<"  |_|_\\_\\___|\\___|\\__,_|_|  |___/_| \\_/ \\___|_              ___             "<<endl;
   cout<<"  |  _ \\  ___  ___ _ __ ___  __ _ ___  ___  | |__  _   _   / _ \\ _ __   ___ "<<endl;
-  cout<<"  | | | |/ _ \\/ __| '__/ _ \\/ _` / __|/ _ \\ | '_ \\| | | | | | | | '_ \\ / _ \\ ";
+  cout<<"  | | | |/ _ \\/ __| '__/ _ \\/ _` / __|/ _ \\ | '_ \\| | | | | | | | '_ \\ / _ \\ " << endl;
    cout<<"  | |_| |  __/ (__| | |  __/ (_| \\__ \\  __/ | |_) | |_| | | |_| | | | |  __/" << endl;
     cout<<"  |____/ \\___|\\___|_|  \\___|\\__,_|___/\\___| |_.__/ \\__, |  \\___/|_| |_|\\___|" << endl;
     cout<<"   __ _ _ __   __| |  / ___|___  _ __   __ _ _   _|___/ _ __               " << endl;
@@ -157,9 +173,8 @@ void title()
     cout<<" | (_| | | | | (_| | | |__| (_) | | | | (_| | |_| |  __/ |                 " << endl;
     cout<<"  \\__,_|_| |_|\\__,_|  \\____\\___/|_| |_|\\__, |\\__,_|\\___|_|                 " << endl;
     cout<<"                                          |_|              "      << endl;          
-  cout << "\nDuring this program you will see the workings of a decrease and conquer recursive algorithm!\n";
-  cout << "As we go along, please press enter to see each step!\n"; 
-  cout << "To start please enter in the size of your array: ";
+  cout << "\nDuring this program you will see the workings of a decrease by one and conquer recursive algorithm!\n";
+  cout << "As we go along, please press enter to see each step!\n\n"; 
 }
 
 
